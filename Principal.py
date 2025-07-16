e6219ce26ba48afb46527b7e5c7a768624e4dccb
erick2990 <e30rick@gmail.com>
2025-07-16 10:59:22 -0600

    funcion de registro de estudiantes terminada

--- Principal.py
+++ Principal.py
@@ -14,7 +14,15 @@ class estudiante: #nombre del objeto estudiante
     def mostrar_datos(self):
         print(f'Nombre: {self.Nombre} Edad: {self.Edad} Carrera: {self.Carrera}')
 
-class curso
+class curso:
+
+    def __init__(self, nota_tarea, nota_parcial, nota_proyecto):
+        self.tarea = nota_tarea
+        self.parcial = nota_parcial
+        self.proyecto = nota_proyecto
+
+    def mostrar_datoscurso(self):
+        print(f'Notas:\nTareas:{self.tarea} Parcial:{self.parcial} Proyecto:{self.proyecto}')
 
 
 def registro_estudiante():
@@ -24,8 +32,26 @@ def registro_estudiante():
     nombre = input('Ingrese el nombre: ')
     edad = input('Ingrese la edad: ')
     carrera = input('Ingrese la carrera del estudiante: ')
+    estudiante_ingreso = estudiante(nombre, edad, carrera) #Instanciamos un objeto temporal
+    cantidad_cursos = int(input('Ingrese la cantidad de cursos que se asignara: '))
+    cursos = {} #se crea un diccionario vacio para este objeto y posterior se llenan
 
+    for i in range(cantidad_cursos):
+        nombre_curso = input('Ingrese el nombre del curso: ')
+        nota_tarea = float(input('Ingrese la nota de tareas: '))
+        nota_parcial = float(input('Ingrese la nota de parcial: '))
+        nota_proyecto = float(input('Ingrese la nota del proyecto: '))
+        curso_tmp = curso(nota_tarea, nota_parcial, nota_proyecto)
 
+        cursos[nombre_curso] = {
+            "Notas" : curso_tmp
+        }
+
+    estudiantes[carnet] = {
+        "Estudiante": estudiante_ingreso,
+        "Cursos": cursos
+
+    }
 
 
 
@@ -42,8 +68,10 @@ while fin_menu:
             registro_estudiante()
 
         case 2:
+            print('Mostrar estudiantes')
 
         case 3:
+            print('Buscar estudiantes')
 
         case 4:
             print('Gracias por usar el sistema :) ')
